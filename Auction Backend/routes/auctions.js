707e59b42b6d74997a78d7276bd944cb7c651465
lakshay bhatia <bhatia00lakshay@gmail.com>
2025-06-21 20:17:34 +0530

    edited routes to remove dynamic routes

--- Auction Backend/routes/auctions.js
+++ Auction Backend/routes/auctions.js
@@ -4,22 +4,26 @@ const db = require("../db");
 
 //gets all the products
 router.get('/',async (req,res) => {
-    const result = await db.query(`SELECT * FROM auctions`);
-    res.json(result.rows);
+    
+    if(!req.body.id){
+        const result = await db.query(`SELECT * FROM auctions`);
+        res.json(result.rows);
+    }
+    else{
+        const id = req.body.id;
+        const result = await db.query(`SELECT * FROM auctions where id = $1`,[id]);
+        res.json(result.rows[0]);
+    }
+    
+    
 })
 
-//fetch product using id
-router.get('/:id',async (req,res) => {
-    const {id} = req.params;
-    const result = await db.query(`SELECT * FROM auctions where id = $1`,[id]);
-    res.json(result.rows[0]);
-})
+
 
 //post bid
-router.post('/:id/bid',async (req,res) => {
+router.post('/bid',async (req,res) => {
     try{
-        const {id} = req.params;
-        const {username,bid_amount} = req.body;
+        const {username,bid_amount,id} = req.body;
         console.log("Incoming auction details --> ID :",id,"Username:",username,"Bid Amount:",bid_amount)
         const auction = await db.query("SELECT * FROM auctions WHERE id=$1",[id]);
         if(!auction.rows.length) {
