65fbc4fd43f1ad860b3537ce9ce69e98b6d2dd0c
lakshay bhatia <bhatia00lakshay@gmail.com>
2025-06-22 11:05:57 +0530

    Test files deleted

--- OmniDim-Config/text.py
+++
@@ -1,6 +0,0 @@
-response = client.call.create(
-    agent_id="your_agent_id_here",
-    phone_number="+1234567890",  # or any other identifier for the user
-    metadata={}  # optional additional data
-)
-print(response)
